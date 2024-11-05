18c8055708197b959201d11449ea5333cef6c7f6
GRIGORSARUKHANYAN <saruxanyan.grigor@gmail.com>
2024-11-05 10:48:17 +0400

    add dock

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TASK-Skilex
+
+step 1 npm i:
+step 2 create .env file add example env file example keys
+step 3 (start project) npm start :
+
+# api dock
+this documentation shows the ready functionality 
+ http://localhost:3000/api-docs/
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import combinationRoutes from "./routes/combination.routes.js";
+import fs from "fs"
+import path from "path"
+import { parse } from "yaml";
+import swaggerUI from "swagger-ui-express";
 
 dotenv.config();
+
+
 const app = express();
 app.use(express.json());
 
 app.use("/api", combinationRoutes);
-
+function setupSwagger() {
+  const fileContents = fs.readFileSync(
+    path.join(path.resolve(), "app.yml"),
+    "utf8"
+  );
+  const doc = parse(fileContents);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(doc));
+}
+setupSwagger()
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
